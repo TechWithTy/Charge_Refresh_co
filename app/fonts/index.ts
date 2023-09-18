9e396ffd35e3cdadb7a59ef5cc3cef945f82f859
nehal7-ml <nehal.sk.99@gmail.com>
2023-09-18 18:34:22 +0530

    fixed font style

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,9 +1,10 @@
 import localFont from "next/font/local";
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 
 export const sfPro = localFont({
   src: "./SF-Pro-Display-Medium.otf",
   variable: "--font-sf",
+
 });
 
 export const inter = Inter({
@@ -11,7 +12,8 @@ export const inter = Inter({
   subsets: ["latin"],
 });
 
-export const mont = localFont({
-  src: './Montserrat-VariableFont_wght.ttf',
+export const mont = Montserrat({
   variable: '--font-mont',
+  subsets: ["latin"],
+  weight: ['300', '400', '500', '700', '900']
 })
