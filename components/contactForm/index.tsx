89d7e5aeb5bc2db12fff5f8c4146cc3111db3a46
nehal7-ml <nehal.sk.99@gmail.com>
2023-10-05 23:37:08 +0530

    fixed contact form

--- components/contactForm/index.tsx
+++ components/contactForm/index.tsx
@@ -41,15 +41,15 @@ function ContactForm() {
         <div className="w-full lg:  h-fit mx-auto p-6 bg-neutral-300 rounded-lg shadow-lg">
             <div className="absolute -z-10 w-40 h-20 top-1/3  -translate-x-1/2   fill-darkblue"><Dots className="fill-white" /></div>
 
-            {showForm && <form className="h-full w-full p-6" onSubmit={handleSubmit}>
+            {showForm && <form className="h-full w-full flex flex-col justify-center lg:block p-6" onSubmit={handleSubmit}>
                 <div className="mb-4 h-2/6">
-                    <p className="mb-2 font-bold h-1/6">I am interested in ...</p>
-                    <div className="flex justify-start flex-wrap gap-5 h-5/6">
+                    <p className="mb-2 text-center lg:text-left font-bold h-1/6">I am interested in ...</p>
+                    <div className="flex justify-center lg:justify-start flex-wrap gap-5 h-5/6">
                         {['Auto Detailing', 'Mobile Charging', 'Becoming a Partner', 'Investing', 'Other'].map((interest, index) => (
                             <button
                                 key={index}
                                 onClick={() => handleInterestClick(interest)}
-                                className={`rounded-lg px-4 py-2 border-2 border-opacity-30 border-purple-800 focus:outline-none ${selectedInterest === interest
+                                className={`h-fit w-fit rounded-lg px-4 py-2 border-2 border-opacity-30 border-purple-800 focus:outline-none ${selectedInterest === interest
                                     ? 'bg-darkblue text-white'
                                     : 'bg-transparent hover:bg-blue-200  text-opacity-30 text-purple-800'
                                     }`}
